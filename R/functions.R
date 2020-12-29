e6b3b080502e5d8e5efb6ddf5b010f4d5ed5fb26
Abasi Latcham <abasi.latcham@gmail.com>
2020-12-29 18:01:54 +1100

    initial functions and documentation

---
+++ R/functions.R
@@ -0,0 +1,48 @@
+#' Convert a drawn box to an SF object
+#'
+#' Take a rectangle drawn on the leaflet, pull out its coords, and turn it into
+#' a single feature polygon
+#'
+#' @param coords from the shiny, the list of coords
+#'
+#' @return
+#' @export
+#'
+#' @examples
+box_to_sf <- function(coords) {
+
+  # Create a sfg polygon
+  pts <- coords %>%
+    purrr::map(unlist) %>%
+    purrr::reduce(rbind)
+  # Adjust for the >180 coords
+  pts <- ifelse(abs(pts) > 180,
+                pts + 360, pts)
+  rownames(pts) <- NULL
+
+  # Make an sf object
+  sf::st_polygon(list(pts)) %>%
+    # sf::st_sfc(crs = 3857)
+    sf::st_sfc(crs = "+proj=longlat +datum=WGS84")
+}
+
+#' Manually build a bounding box using the Shiny rectangle
+#'
+#' Manually construct the bounding box and return a vector.
+#'
+#' @param coords from the shiny
+#'
+#' @return
+#' @export
+#'
+#' @examples
+manual_bbox <- function(coords) {
+  # the + 360 is probably not always valid
+  x_min <- coords[[1]][[1]] + 360
+  x_max <- coords[[3]][[1]] + 360
+  y_min <- coords[[1]][[2]]
+  y_max <- coords[[3]][[2]]
+  bb <- rbind(c(x_min, x_max), c(y_min, y_max))
+  dimnames(bb) <- list(c("x", "y"), c("min", "max"))
+  return(bb)
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# bbb
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of bbb is to ...
+
+## Installation
+
+You can install the released version of bbb from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("bbb")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(bbb)
+## basic example code
+```
+
