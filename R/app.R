0da891d4c0f6d37eac336193bc3fc5493ec76adf
Abasi Latcham <abasi.latcham@gmail.com>
2021-01-03 21:11:23 +1100

    tweaks layout, assigns sf to global env

--- R/app.R
+++ R/app.R
@@ -1,10 +1,20 @@
 app <- function() {
   shiny::shinyApp(
-    ui = shiny::fluidPage(shiny::actionButton("button",
-                                              "Build my box"),
-                          leaflet::leafletOutput('map')
-                          ),
-
+    ui = shiny::fluidPage(
+      shiny::titlePanel(title = "Bounding Box Builder"),
+      shiny::sidebarLayout(
+        shiny::sidebarPanel(
+          shiny::textInput(inputId = "var_name",
+                           label = "Name of the bb var",
+                           value = "bbox"),
+          shiny::actionButton(inputId = "button",
+                              label = "Build my box")
+          ),
+      shiny::mainPanel(
+        leaflet::leafletOutput(outputId = "map")
+        )
+      )
+    ),
     server = function(input, output) {
       output$map = leaflet::renderLeaflet({
         leaflet::leaflet() %>%
@@ -21,7 +31,16 @@ app <- function() {
       })
 
       shiny::observeEvent(input$button, {
-        test <<- input$map_draw_all_features$features[[1]]
+        # Get the coords from the box
+        coords <- input$map_draw_all_features$features[[1]]$geometry$coordinates[[1]]
+        # Turn into an sf
+        box <- box_to_sf(coords)
+        # Assign to something in the global
+        # Need to add check that var_name exists
+        assign(x = input$var_name,
+               value = box,
+               envir = .GlobalEnv)
+        shiny::stopApp()
       })
     }
   )
